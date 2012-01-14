3bc0e49c406d57a76e21c9ae4e4ea3a007d7919a
nulltask <seiya@uniba.jp>
2012-01-14 19:40:43 +0900

    refactoring...

--- app.js
+++ app.js
@@ -2,80 +2,86 @@
  * Module dependencies.
  */
 require.paths.unshift('./node_modules');
-var express = require('express');
-var app = module.exports = express.createServer(express.logger(), express.bodyParser());
-var clientId = process.env.INSTAGRAM_CLIENT_ID,
-	clientSecret = process.env.INSTAGRAM_CLIENT_SECRET;
-var Instagram = require('instagram-node-lib');
-Instagram.set('client_id', clientId);
-Instagram.set('client_secret', clientSecret);
-var _ = require('underscore');
-var delayTime = 0;
 
-console.log(process.env);
-console.log([clientId, clientSecret]);
+var express = require('express')
+  , app = module.exports = express.createServer(express.logger(), express.bodyParser())
+  , clientId = process.env.INSTAGRAM_CLIENT_ID
+  , clientSecret = process.env.INSTAGRAM_CLIENT_SECRET
+  , Instagram = require('instagram-node-lib')
+  , _ = require('underscore')
+  , delayTime = 0;
+
 
 // Configuration
 app.configure(function() {
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	// use stylus
-	app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-	app.use(app.router);
-	app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src : __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+  Instagram.set('client_id', clientId);
+  Instagram.set('client_secret', clientSecret);
 });
+
 app.configure('development', function() {
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({
+    dumpExceptions : true
+  , showStack : true
+  }));
 });
+
 app.configure('production', function() {
-	app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 app.dynamicHelpers({
-	title: function() {
-		return 'infinigram';
-	},
-	p: function(req, res) {
-		return req.params;
-	},
-	q: function(req, res) {
-		return req.query;
-	},
-	e: function(req, res) {
-		return process.env;
-	}
+  title : function() {
+    return 'infinigram';
+  },
+  p: function(req, res) {
+    return req.params;
+  },
+  q: function(req, res) {
+    return req.query;
+  },
+  e: function(req, res) {
+    return process.env;
+  }
 });
 
 // Routes
 app.get('/', function(req, res) {
-	res.render('index');
+  res.render('index');
 });
 
 app.get("/tags/:tag/media/recent.html", function(req, res) {
-	var query = {};
-	query.min_id = req.query.min_id;
-	query.max_id = req.query.max_id;
-//	console.log(req.query);
-	Instagram.tags.recent(_.extend({
-		name: req.params.tag,
-		complete: function(data, pagination) {
-			// console.log([ pagination, data ]);
-			// console.log(pagination);
-			setTimeout(function() {
-				res.render("list", { data: data, pagination: pagination });
-			}, delayTime);
-		},
-		error: function(errorMessage, errorObject, caller) {
-			// console.log([errorMessage, errorObject, caller]);
-			res.send(500);
-		}
-	}, query));
+  var query = {};
+  query.min_id = req.query.min_id;
+  query.max_id = req.query.max_id;
+  //	console.log(req.query);
+  Instagram.tags.recent(_.extend({
+    name: req.params.tag,
+    complete: function(data, pagination) {
+      // console.log([ pagination, data ]);
+      // console.log(pagination);
+      setTimeout(function() {
+        res.render("list", {
+          data : data,
+          pagination : pagination
+        });
+      }, delayTime);
+    },
+    error : function(errorMessage, errorObject, caller) {
+      // console.log([errorMessage, errorObject, caller]);
+      res.send(500);
+    }
+  }, query));
 });
 
 // Only listen on $ node app.js
 if (!module.parent) {
-	app.listen(process.env.VMC_APP_PORT || 3000);
-	// console.log("Express server listening on port %d", app.address().port);
+  app.listen(process.env.VMC_APP_PORT || 3000);
+  // console.log("Express server listening on port %d", app.address().port);
 }
